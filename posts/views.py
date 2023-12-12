8be406bf73f71f1101ffc0f1d7df98f2f1642177
loay fayez <84818419+leon019i@users.noreply.github.com>
2023-12-12 02:07:26 +0200

    initial commit

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from django.views.generic import ListView
+from .models import Post
+from django.views.generic import ListView
+
+from .models import Post
+
+
+# Create your views here.
+
+
+class HomePageView(ListView):
+    model = Post
+    template_name = "posts/home.html"
